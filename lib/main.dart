608144df9ba8e3c17399ed5dc51ea5aa84243c16
osem256 <osem16@gmail.com>
2022-03-30 14:18:53 +0300

    no changes

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: ' My first app '),
     );
   }
 }
